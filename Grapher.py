322c7d03ac32268be1a0a0ef429cbb2cbfdff93f
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-06-17 13:53:49 -0700

    Update Grapher.py

--- Grapher.py
+++ Grapher.py
@@ -4,11 +4,24 @@ import random as rand
 def comlexFunction(complexInput):
     return complexInput**2
 
-print("initializing...")
-imageData = []
-for pixel in range(1920*1080):
-    imageData.append(complex(rand.random(), rand.random()))
-print("image list created...")
-for pixel in range(0,len(imageData)):
-    imageData[pixel] = (pixel % 1920 - 1080 / 2) // 2
-print("actually done now!")
+class graph:
+    def __init__(self, equations = []
+
+
+
+
+
+
+
+
+
+
+
+#print("initializing...")
+#imageData = []
+#for pixel in range(1920*1080):
+#    imageData.append(complex(rand.random(), rand.random()))
+#print("image list created...")
+#for pixel in range(0,len(imageData)):
+#    imageData[pixel] = (pixel % 1920 - 1080 / 2) // 2
+#print("actually done now!")
