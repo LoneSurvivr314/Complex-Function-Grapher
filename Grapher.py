119fc2b80bdbdc3f202e338282747687b76469af
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-06-17 15:02:00 -0700

    Update Grapher.py

--- Grapher.py
+++ Grapher.py
@@ -5,8 +5,10 @@ def comlexFunction(complexInput):
     return complexInput**2
 
 class graph:
-    def __init__(self, equations = []
-
+    def __init__(self, xmin = -1, xmax = 1, ymin = -9/16, ymax = -9/16,
+                 width = 1920, height = 1080,
+                 equation = lambda z: z):
+        pass
 
 
 
