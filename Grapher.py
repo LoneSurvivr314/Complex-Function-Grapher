1a9f4d089f9f12abe5d7a99106c466dcf8d41d36
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-06-17 08:24:12 -0700

    Create .gitignore

--- Grapher.py
+++ Grapher.py
@@ -1,1 +1,14 @@
+import cmath
+import random as rand
 
+def comlexFunction(complexInput):
+    return complexInput**2
+
+print("initializing...")
+imageData = []
+for pixel in range(1920*1080):
+    imageData.append(complex(rand.random(), rand.random()))
+print("image list created...")
+for pixel in range(0,len(imageData)):
+    imageData[pixel] = (pixel % 1920 - 1080 / 2) // 2
+print("actually done now!")
