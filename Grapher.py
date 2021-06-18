9b3cf4f2031d1b5eb92e226448edaf1de324fc02
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-06-18 13:15:32 -0700

    Update Grapher.py

--- Grapher.py
+++ Grapher.py
@@ -1,6 +1,6 @@
 import cmath
 import random as rand
-
+from PIL import Image
 def comlexFunction(complexInput):
     return complexInput**2
 
@@ -8,24 +8,22 @@ class graph:
     def __init__(self, xmin = -1, xmax = 1, ymin = -9/16, ymax = -9/16,
                  width = 1920, height = 1080,
                  equation = lambda z: z):
-        self.size = {"width":width,"height":height}
+        self.width = width
+        self.height = height
         self.view = {"xmin":xmin,"xmax":xmax,"ymin":ymin,"ymax":ymax}
         self.equation = equation
+        self.image = Image.new(mode = "RGB", size = (self.width, self.height))
+        self.data = []
     
+    def render(self):
+        print("starting")
+        self.data = []
+        for y in range(self.height):
+            for x in range(self.height):
+                self.data.append(self.equation(complex(
+                    x/self.width*(self.view["xmax"]-self.view["xmin"])+self.view["xmin"],
+                    y/self.height*(self.view["ymax"]-self.view["ymin"])+self.view["ymin"]
+                    )))
 
-
-
-
-
-
-
-
-
-#print("initializing...")
-#imageData = []
-#for pixel in range(1920*1080):
-#    imageData.append(complex(rand.random(), rand.random()))
-#print("image list created...")
-#for pixel in range(0,len(imageData)):
-#    imageData[pixel] = (pixel % 1920 - 1080 / 2) // 2
-#print("actually done now!")
+my_graph = graph()
+my_graph.render()
