94ff54f23d87a23a19f6cbc30fe9c929593874cc
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-07-02 14:46:11 -0700

    Overhauled rendering system
    Added keyframe class that handles each keyframe's data

--- Grapher.py
+++ Grapher.py
@@ -8,16 +8,13 @@ def comlexFunction(complexInput):
 def rephase(phase): # Remaps the phase of a number from (-pi,pi) to (0,2pi)
         return(phase if phase >= 0 else phase + 2*cmath.pi)
 
-class graph:
-    def __init__(self, xmin = -1, xmax = 1, ymin = -9/16, ymax = 9/16,
-                 width = round(1920/4), height = round(1080/4),
+class keyframe:
+    def __init__(self, parent, xmin = -1, xmax = 1, ymin = -9/16, ymax = 9/16,
                  equation = lambda z: (1j)**(z**2)):
-        self.width = width
-        self.height = height
         self.view = {"xmin":xmin,"xmax":xmax,"ymin":ymin,"ymax":ymax}
         self.equation = equation
-        self.image = Image.new(mode = "HSV", size = (self.width, self.height))
         self.data = []
+        self.image = Image.new("HSV", (parent.width, parent.height))
     
     def complexToHSV(self, complex):
         phase = rephase(cmath.phase(complex))
@@ -27,23 +24,36 @@ class graph:
             round(256 - 64 * (math.log(abs(complex),2) % 1))
             ))
 
-    def render(self, show = True):
+    def render(self, parent, show = True):
         print("starting")
         self.data = []
-        for y in range(self.height):
-            for x in range(self.width):
+        for y in range(parent.height):
+            for x in range(parent.width):
                 self.data.append(
                     self.complexToHSV(self.equation(complex(
-                    x/self.width*(self.view["xmax"]-self.view["xmin"])+self.view["xmin"],
-                    y/self.height*(self.view["ymax"]-self.view["ymin"])+self.view["ymin"]
+                    x/parent.width*(self.view["xmax"]-self.view["xmin"])+self.view["xmin"],
+                    y/parent.height*(self.view["ymax"]-self.view["ymin"])+self.view["ymin"]
                     ))))
         self.image.putdata(self.data)
         print(str(len(self.data)) + " points calculated")
-        #textOutput = open(r"C:\Users\jeffr\Desktop\testOutput.txt","w")
-        #textOutput.write(str(self.data))
+        
         if show:
             self.image.show()
 
 
-my_graph = graph(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, width = 960, height = 540)
-my_graph.render(show = True)
+class graph:
+    def __init__(self, width = 480, height = 270):
+        self.keyframes = []
+        self.width = width
+        self.height = height
+
+    def addKeyframe(self, xmin = -1, xmax = 1, ymin = -9/16, ymax = 9/16,
+                    equation = lambda z: (1j)**(z**2)):
+        self.keyframes.append(keyframe(self, xmin, xmax, ymin, ymax, equation))
+
+    def preview(self, keyframe = 0):
+        self.keyframes[0].render(self, show = True)
+
+my_graph = graph(width = 960, height = 540)
+my_graph.addKeyframe(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, equation = lambda z: (1j)**(z**2))
+my_graph.preview(keyframe= 0)
