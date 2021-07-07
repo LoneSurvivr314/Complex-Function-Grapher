96ca5b2e7d755f6ddec501246449a366fdb03c1a
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-07-07 14:39:03 -0700

    Update Grapher.py

--- Grapher.py
+++ Grapher.py
@@ -2,12 +2,15 @@ import cmath
 import math
 import random as rand
 from PIL import Image
+from itertools import repeat
 
 def rephase(phase): # Remaps the phase of a number from (-pi,pi) to (0,2pi)
         return(phase if phase >= 0 else phase + 2*cmath.pi)
 
 def weightedAverage(num1, num2, weight): # weighted average of num1 and num2, weight is between 0 and 1
-    return(num1 - weight * (num1 + num2))
+    #print(num1, num2, weight)
+    #print("----")
+    return(num1 - weight * (num1 - num2))
 
 def complexToHSV(complex): #return (H,S,V) tuple from a complex input
     phase = rephase(cmath.phase(complex))
@@ -20,11 +23,11 @@ def complexToHSV(complex): #return (H,S,V) tuple from a complex input
     except ValueError:
         return((0, 224, 192))
 
-def render(data, width, height, show = True):
+def render(data, width, height, show = True): # render data by mapping complexToHSV over it
     print("mapping data")
     print("mapped data")
     image = Image.new("HSV", (width, height))
-    image.putdata(list(map(complexToHSV, data))) #if data is calculated, map complexToHSV over the data and write data to image
+    image.putdata(list(map(complexToHSV, data)))
     if show:
         image.show()
 
@@ -76,13 +79,18 @@ class graph:
         print("calculating keyframes")
         for keyframe in self.keyframes: # calculate data for all keyframes
             keyframe.calculateData()
-        for keyframe in range(1, len(self.keyframes)-1):
-            print("rendering frames")
-            for frame in range(keyframe.numberOfFrames):
-                keyframe.render(data = map(weightedAverage(self.keyframes[keyframe - 1], self.keyframes[keyframe], weight = frame / self.keyframes[keyframe])), show = True)
+        for keyframeNum in range(1, len(self.keyframes)):
+            numberOfFrames = self.keyframes[keyframeNum].numberOfFrames
+            firstData = self.keyframes[keyframeNum - 1].data
+            secondData = self.keyframes[keyframeNum].data
+            for frame in range(1, numberOfFrames + 1):
+                render(
+                       data = map(weightedAverage, firstData, secondData, repeat(frame / numberOfFrames)),
+                       width = self.width, height = self.height, show = True)
+                print(frame / numberOfFrames)
         
 my_graph = graph(width = 480, height = 270)
 my_graph.addKeyframe(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, equation = lambda z: z)
 my_graph.addKeyframe(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, equation = lambda z: (1j)**(z**2), numberOfFrames = 10)
-my_graph.calculateKeyframe(0)
-my_graph.preview(0)
+my_graph.renderAnimation()
+#print(weightedAverage(0,1,1))
