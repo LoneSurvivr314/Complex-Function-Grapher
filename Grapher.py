6aa2cf76302f2b52ad43270ec9ea3c4d7f3f84f3
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-07-06 20:26:46 -0700

    Update Grapher.py

--- Grapher.py
+++ Grapher.py
@@ -9,6 +9,25 @@ def rephase(phase): # Remaps the phase of a number from (-pi,pi) to (0,2pi)
 def weightedAverage(num1, num2, weight): # weighted average of num1 and num2, weight is between 0 and 1
     return(num1 - weight * (num1 + num2))
 
+def complexToHSV(self, complex): #return (H,S,V) tuple from a complex input
+    phase = rephase(cmath.phase(complex))
+    try:
+        return((
+            round(phase * 40.7436654315), #change angle from (0, 2pi) to (0,256)
+            round(256 - 32 * (math.log(abs(complex), 2) % 1)),
+            round(256 - 64 * (math.log(abs(complex), 2) % 1))
+            ))
+    except ValueError:
+        return((0, 224, 192))
+
+def render(data, width, height, show = True):
+    print("mapping data")
+    print("mapped data")
+    image = Image.new("HSV", (width, height))
+    image.putdata(list(map(complexToHSV, data))) #if data is calculated, map complexToHSV over the data and write data to image
+    if show:
+        image.show()
+
 class keyframe:
     # a keyframe holds data about view are and equation, and knows how to render itself.
     def __init__(self, parent, xmin = -1, xmax = 1, ymin = -9/16, ymax = 9/16,
@@ -21,16 +40,6 @@ class keyframe:
         self.image = Image.new("HSV", (self.width, self.height)) # the image that
         self.numberOfFrames = int(numberOfFrames)
     
-    def complexToHSV(self, complex): #return (H,S,V) tuple from a complex input
-        phase = rephase(cmath.phase(complex))
-        try:
-            return((
-                round(phase * 40.7436654315), #change angle from (0, 2pi) to (0,256)
-                round(256 - 32 * (math.log(abs(complex), 2) % 1)),
-                round(256 - 64 * (math.log(abs(complex), 2) % 1))
-                ))
-        except ValueError:
-            return((0, 224, 192))
 
     def calculateData(self): #calculate complex numbers for each point supplied
         for y in range(self.height):
@@ -41,18 +50,7 @@ class keyframe:
                     y/self.height*(self.view["ymax"]-self.view["ymin"])+self.view["ymin"]
                     )))
         
-    def render(self, data, show = True):
-        if data == []: #if data is not calculated yet, calculate, then run render again
-            print("calculating data")
-            self.calculateData()
-            print("calculated data")
-            self.render(data, show = show)
-        else:
-            print("mapping data")
-            print("mapped data")
-            self.image.putdata(list(map(self.complexToHSV, self.data))) #if data is calculated, map complexToHSV over the data and write data to image
-            if show:
-                self.image.show()
+
 
 
 class graph:
@@ -63,11 +61,15 @@ class graph:
         self.height = height
 
     def addKeyframe(self, xmin = -1, xmax = 1, ymin = -9/16, ymax = 9/16,
-                    equation = lambda z: (1j)**(z**2)):
-        self.keyframes.append(keyframe(self, xmin, xmax, ymin, ymax, equation))
+                    equation = lambda z: (1j)**(z**2), numberOfFrames = 60):
+        self.keyframes.append(keyframe(self, xmin, xmax, ymin, ymax, equation, numberOfFrames))
 
     def preview(self, keyframe = 0):
-        self.keyframes[keyframe].render(self.keyframes[keyframe].data, show = True)
+        render(data = self.keyframes[keyframe].data, width = self.width, height = self.height, show = True)
+    
+    def calculateKeyframe(self, keyframeNumber = 0):
+        print("calculating data")
+        self.keyframes[keyframeNumber].calculateData
 
     def renderAnimation(self):
         print("calculating keyframes")
@@ -81,6 +83,5 @@ class graph:
 my_graph = graph(width = 480, height = 270)
 my_graph.addKeyframe(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, equation = lambda z: z)
 my_graph.addKeyframe(xmin = -2, xmax = 2, ymin = -9/8, ymax = 9/8, equation = lambda z: (1j)**(z**2), numberOfFrames = 10)
-my_graph.renderAnimation()
-#my_graph.preview(0)
-#my_graph.preview(1)
+my_graph.calculateKeyframe(0)
+my_graph.preview(0)
