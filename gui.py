48c128430a99edbcb83229d5a44433c833936a11
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2021-07-14 21:37:47 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,11 +1,18 @@
+from tkinter.constants import TRUE
 import PySimpleGUI as sg
 
-layout = [[sg.Button("Add keyframe"), sg.Button("Preview Keyframe")],[sg.Button("Render Animation")]]
+layout = [[sg.Button("Add keyframe"), sg.Button("Preview Keyframe")],
+          [sg.Button("Render Animation")]]
 
 window = sg.Window('Animation Editor', layout)
 
-event, values = window.read()
+while True:
+    event, values = window.read()
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+
+    if event == "Add keyframe":
+        sg.popup_get_text("Enter equation")
+    print(event, values)
 
 window.close()
-
-sg.Popup(event, values)
